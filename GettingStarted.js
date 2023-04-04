7b6291cb26e28652a5d4ee3fe31f8adc421b8062
Sarthak Bakshi <123444624+sarthakbakshicode@users.noreply.github.com>
2023-04-04 17:59:58 +0400

    Update GettingStarted.js

--- GettingStarted.js
+++ GettingStarted.js
@@ -1,1 +1,46 @@
+Getting Started
+Install the SDK: To get started with the XDC SDK, you'll need to install it on your local machine. To do this, simply run the following command in your terminal:
+
+npm install xdc-sdk --save
+
+
+Create a new project: Once the SDK is installed, you can create a new project using your favorite code editor. In this example, we'll be using Visual Studio Code.
+
+Import the SDK: In order to use the XDC SDK in your project, you'll need to import it. To do this, add the following line of code to the top of your JavaScript or TypeScript file:
+import * as XdcSdk from 'xdc-sdk';
+
+Connect to the network: Before you can start interacting with the XDC network, you'll need to connect to it. To do this, you'll need to create a new XdcSdk.providers.JsonRpcProvider instance and pass it the URL of the network you want to connect to. For example:
+
+const provider = new XdcSdk.providers.JsonRpcProvider('https://rpc.apothem.network');
+
+Create an account: Next, you'll need to create an account that you can use to interact with the XDC network. To do this, you can use the XdcSdk.Wallet.createRandom method, which will generate a new account with a random private key. For example:
+
+const wallet = XdcSdk.Wallet.createRandom();
+
+
+Deploy a contract: Now that you have an account, you can deploy a contract to the XDC network. To do this, you'll need to write a Solidity contract and compile it using a tool like Remix. Once you have a compiled contract, you can deploy it using the XdcSdk.ContractFactory and XdcSdk.Contract classes. For example:
+const contractFactory = new XdcSdk.ContractFactory(abi, bytecode, wallet);
+const contract = await contractFactory.deploy();
+await contract.deployTransaction.wait();
+console.log('Contract deployed at address:', contract.address);
+
+Interact with the contract: Finally, you can interact with the contract by calling its methods using the XdcSdk.Contract class. For example:
+const message = await contract.functions.getMessage();
+console.log('Message:', message);
+
+const newMessage = 'Hello, XDC!';
+const tx = await contract.functions.setMessage(newMessage);
+await tx.wait();
+console.log('New message set:', newMessage);
+
+
+
+
+
+
+
+
+
+
+
 
